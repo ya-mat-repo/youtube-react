be9cd1967661b1940233dbbfaca4ae41922449b2
m_u_say@hotmail.com <m_u_say@hotmail.com>
2021-07-28 01:20:28 +0900

    setting youtube api and test to get data from youtube

---
+++ src/apis/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const KEY = 'AIzaSyA5BcPNFTA-C8D83yIheZaF2eOqaBbcoM0'
+
+const youtube = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3'
+})
+
+export const fetchPopularData = async () => {
+  return await youtube.get('/videos', {
+    params: {
+      part: 'snippet',
+      maxResults: 40,
+      key: KEY,
+      regionCode: 'JP',
+      type: 'video',
+      chart: 'mostPopular'
+    }
+  })
+}
